571a4ff1164c4ef267f0b5987a70232451e9fb7d
ytpremiumcr7-collab <ytpremiumcris7@gmail.com>
2026-09-19 13:50:21 -0600

    Corpus estatal: barrel de 104 documentos (32 estados)

---
+++ src/data/estatal.ts
@@ -0,0 +1,5 @@
+import { ESTATAL_A } from "./estatal-a";
+import { ESTATAL_B } from "./estatal-b";
+import { ESTATAL_C } from "./estatal-c";
+
+export const ESTATAL = [...ESTATAL_A, ...ESTATAL_B, ...ESTATAL_C];
